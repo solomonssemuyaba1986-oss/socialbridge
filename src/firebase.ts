1db4d2bddfea964100f1289a98a97e4b4769210f
solomonssemuyaba1986-oss <solomonssemuyaba1986@gmail.com>
2026-05-09 18:54:46 +0100

    Update firebase.ts
    update firebase config

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,28 +1,17 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCtCgwgi8LoqvMl4oDI-npb63A1GvvFbvk",
-  authDomain: "socialbridge-93ee1.firebaseapp.com",
-  projectId: "socialbridge-93ee1",
-  storageBucket: "socialbridge-93ee1.firebasestorage.app",
-  messagingSenderId: "287573785988",
-  appId: "1:287573785988:web:14227d58017b47b40171d1",
-  measurementId: "G-KHMKXFQ1RG"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-// Initialize Firestore
 export const db = getFirestore(app);
-
-// Initialize Auth
 export const auth = getAuth(app);
 export const googleProvider = new GoogleAuthProvider();
