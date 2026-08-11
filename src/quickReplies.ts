3a4ae2ac356bf681f057045c24d8aaa8399b0605
solomonssemuyaba1986-oss <solomonssemuyaba1986@gmail.com>
2026-08-11 21:59:24 +0100

    Create quickReplies.ts
    add a quick replies file to rachett

---
+++ src/quickReplies.ts
@@ -0,0 +1,13 @@
+export const QUICK_REPLIES = [
+  'Is this still available?',
+  'Do you deliver to my area?',
+  'Can you share more photos?',
+  "What's your best price?",
+  'How long does delivery take?',
+  "What's the payment method?",
+  'Do you have other colors or sizes?',
+  'Do you give discounts for bulk orders?',
+"what's your last price?",
+]
+
+export const QUICK_REPLY_FLASH = '⚡'
